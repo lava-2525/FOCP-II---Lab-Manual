2da9bfb3072afa4bbc86a9df412a3779de8fd321
lava-2525 <lava.gupta2525@gmail.com>
2026-05-20 18:58:29 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int r;
+    float area;
+    cout<<"Enter the radius of the circle: ";
+    cin>>r;
+    area = 3.14*r*r;
+    cout<<"The area of the circle is: "<<area;
+
+    return 0;
+}
