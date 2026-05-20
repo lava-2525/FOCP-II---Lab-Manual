fb71220c1340eef68d22ab5353f9367ee97d7bdb
lava-2525 <lava.gupta2525@gmail.com>
2026-05-20 19:00:21 +0530

    Create q23.cpp

---
+++ q23.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Pattern{
+public:
+    void butterfly(int n){
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++)
+                cout<<"*";
+
+            for(int j=1;j<=2*(n-i);j++)
+                cout<<" ";
+
+            for(int j=1;j<=i;j++)
+                cout<<"*";
+
+            cout<<endl;
+        }
+        for(int i=n-1;i>=1;i--){
+
+            for(int j=1;j<=i;j++)
+                cout<<"*";
+
+            for(int j=1;j<=2*(n-i);j++)
+                cout<<" ";
+
+            for(int j=1;j<=i;j++)
+                cout<<"*";
+
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    Pattern p;
+    p.butterfly(5);
+}
