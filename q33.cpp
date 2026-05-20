f8b898166c17ff6202d495569aca70ab65c4186a
lava-2525 <lava.gupta2525@gmail.com>
2026-05-20 19:04:20 +0530

    Create q33.cpp

---
+++ q33.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[3][5];
+
+    
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter marks for Student " << i + 1 << ":\n";
+
+        for (int j = 0; j < 5; j++)
+        {
+            cout << "Subject " << j + 1 << ": ";
+            cin >> marks[i][j];
+        }
+
+        cout << endl;
+    }
+
+    
+    cout << "Marks in 2nd subject of 1st student: "
+         << marks[0][1] << endl;
+
+    cout << "Marks in 5th subject of 3rd student: "
+         << marks[2][4] << endl;
+
+    return 0;
+}
